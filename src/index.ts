850c31bd129b13db7d18ebaaf922a9286931fb12
Adam Laiacano <adam.laiacano@gmail.com>
2023-04-03 06:36:12 -0400

    add gen subcommand

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,18 @@ const figlet = require("figlet");
 const program = new Command();
 
 program
+  .name("Changelog Util")
   .version("1.0.0")
-  .description("A CLI for managing changelogs")
+  .description("A CLI for managing changelogs");
+
+type GenType = {
+  yamlFile?: string | boolean;
+  output?: string | boolean;
+};
+// Generate a changelog from yaml
+program
+  .command("gen")
+  .name("gen")
   .option(
     "-y, --yaml-file  [file]",
     "The yaml file to be converted to Markdown. Default is changelog.yaml."
@@ -19,28 +29,34 @@ program
     "-o, --output [file]",
     "The output destination, if desired. If not supplied, the markdown will print to stdout"
   )
-  .parse(process.argv);
-
-const options = program.opts();
-
-// check if the option has been used the user
-if (options.yamlFile) {
-  const filepath =
-    typeof options.yamlFile === "string" ? options.yamlFile : "changelog.yaml";
-
-  if (!fs.existsSync(filepath)) {
-    console.error(`No file found: ${filepath}`);
-    process.exit(1);
-  }
-
-  yamlToMarkdown(filepath).then((md: string) => {
-    if (options.output && typeof options.output === "string") {
-      fs.writeFileSync(options.output, md);
-    } else {
-      console.log(md);
+  .action((str: GenType) => {
+    const { yamlFile, output } = str;
+    if (!yamlFile && !output) {
+      program.commands.find((c: any) => c._name === "gen").help();
+      process.exit(0);
     }
+
+    const filepath = typeof yamlFile === "string" ? yamlFile : "changelog.yaml";
+
+    if (!fs.existsSync(filepath)) {
+      console.error(`No file found: ${filepath}`);
+      process.exit(1);
+    }
+
+    yamlToMarkdown(filepath).then((md: string) => {
+      if (output && typeof output === "string") {
+        fs.writeFileSync(output, md);
+      } else {
+        console.log(md);
+      }
+    });
   });
-}
+
+program.showHelpAfterError();
+
+program.parse(process.argv);
+
+// const options = program.opts();
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
